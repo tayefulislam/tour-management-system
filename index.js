94ec4d702c2b1af16fb4578be00730e35a4b027c
tayefulislam <kazitayefulislam12@gmail.com>
2022-10-08 20:01:53 +0600

    Scheman added

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 const app = require("./app");
+const uri = process.env.URI;
+// console.log(uri);
+
+mongoose.connect(uri, () => {
+  console.log("data base connect successful");
+});
 
 app.listen(port, () => {
   console.log("server is runnig at", port);
---
+++ models/Tour.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const TourSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "please enter a name"],
+    trim: true,
+    unique: [true, "Name must be unique"],
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: [0, "Price can't be nagative number"],
+  },
+  details: {
+    type: String,
+    required: true,
+  },
+  totalView: {
+    type: Number,
+    min: [0, "Total view Cannot be nagative number"],
+  },
+});
